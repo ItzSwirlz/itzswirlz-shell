977d6adff705eab105941ca8717c1c6078e15a2f
Joshua Peisach <joshuapeisach600@gmail.com>
2020-08-11 10:53:22 -0400

    Initial release: 0.1~devel
    Features:
    Initial release.
    
    TODO:
    Get Date & Time before main shell loop begins.
    Have support with directories (changing directories, moving, etc.)
    Fix on empty line having a blank command with the shell command being "| lolcat", which will result in error.

---
+++ itzswirlz-shell.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+#
+# ItzSwirlz Shell (c) Joshua Peisach 2020
+# Contact Email: itzswirlz2020@outlook.com
+# ---------------------------------------------------------------
+# This is a shell for ItzSwirlz, but you may use it
+# however you want for your own personal needs.
+#
+# This software is licensed under the GNU Public License V3.
+# You may open issues, pull requests, or create forks at the
+# GitHub repository at:
+# https://github.com/ItzSwirlz/itzswirlz-shell
+#
+#
+# ItzSwirlz Shell: File Description
+# This file contains the main overall script for the shell.
+# This is basically the thing that will let you type in commands
+# and will actually execute them.
+# ---------------------------------------------------------------
+
+sub start {
+  print "ItzSwirlz Shell v0.1~devel (c) GPL v3 Joshua Peisach 2020\n";
+  print "Welcome to the ItzSwirlz Shell!\n";
+}
+
+sub startStatistics {
+  print "OS Statistics:\n";
+  system("neofetch | lolcat");
+}
+
+sub getDateTime {
+  # TODO: Print date/time.
+  # This will be shown before main loop begins.
+  $date = "YYYY-MM-DD HH:MM:SS ZZZZ"
+}
+
+sub main {
+  # The infinite loop that does everything.
+  $cmd = <STDIN>;
+  chop($cmd);
+  system("$cmd | lolcat");
+  main();
+}
+
+start();
+startStatistics();
+main();
